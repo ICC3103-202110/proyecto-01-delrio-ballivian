b694293811e1820ad11bf169a3c3aa2c6f55e1ad
lvdelrio <lvdelrio@miuandes.cl>
2021-04-17 19:43:08 -0400

    actualizacion #1

---
+++ player.py
@@ -0,0 +1,20 @@
+#clase jugador
+class Player:
+
+    def __init__(self,name):
+        self.name = name
+        self.turn = 0
+        self.coins = 0
+
+    #metodos/funciones
+
+    def assasin(self):
+        return 0
+    def duke(self):
+        return 0
+    def abassador(self):
+        return 0
+    def captain(self):
+        return 0
+    def contessa(self):
+        return 0
