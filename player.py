5552a0d31e7e0722ae1386ec4ccbe0d2ec58a8c6
lvdelrio <lvdelrio@miuandes.cl>
2021-04-20 00:48:09 -0400

    actualizacion #3.0.0

--- player.py
+++ player.py
@@ -1,9 +1,22 @@
 #clase jugador
+
+
+from cartas import *
 class Player:
 
-    def __init__(self,name):
-        self.name = name
+    def __init__(self):
+        #self.name = name
         self.turn = 0
         self.coins = 2
+
         self.alive = True
+
+    def in_game(self):
+        card1=Mazo.select_card()
+        card2=Mazo.select_card()
+        self.cards_influece=[card1,card2]
+player=Player()
+print("carta 1 y carta 2",player.in_game())
+
+
 
