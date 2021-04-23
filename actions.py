7c0308db0bcb65415d32c7201faef93ac5d1d98c
AgBallivian <agballivian@gmail.com>
2021-04-23 03:00:18 -0400

    Actualizacion #6.1.1

--- actions.py
+++ actions.py
@@ -15,16 +15,20 @@ class Actions:
     #funcion que evalua el desafio y a quien quitar carta, la llamo en contraataque
     #tienes que juntar tu lista hidden con las cartas reveladas aqui
     def quitar_carta(self,player, player_list, playern, name):
-        print(playern, "PLAYERN EN QUITAR CARTA---------------------")
+        #print(playern, "PLAYERN EN QUITAR CARTA---------------------")
+        #print(player.name_id ,"Jugador daaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+        print(name, "= duke")
+        
         screen_update = Screen_update(player_list)
         cardloose_select = tki.Tk()
         cardloose_select.title("Coup")
         cardloose_select.geometry("300x200")
 
-        def cardloose(card): #VER BIEN PARA VER CUALES SI ESTAN PERDIDAS, ACTUALMENTE SOLO ESTAN EN player.show[] -----------------------------------
+        def cardloose(card, player, player_list, name): #VER BIEN PARA VER CUALES SI ESTAN PERDIDAS, ACTUALMENTE SOLO ESTAN EN player.show[] -----------------------------------
             cardloose_select.destroy()
-
-            if(name == "duke" or name == "coup" or name == "foreingn_aid"):
+            #print(name, "NAMWEEERWEIWEEUIKUIEI92381890123128932183091========================")
+            if name == "duke" or name == "coup" or name == "foreingn_aid" or name == "assassin" :
+                print(player.name_id ,"Jugador decente por facasdsah jkdasjkgdfjkasjf jsatjkftg sa")
                 #Lose card 0
                 if card == player.cards[0]:
                     player.show[0] = player.cards[0]  #Reemplazo el show
@@ -35,41 +39,42 @@ class Actions:
                     player.cards[1] = " "
                     
             elif name == "captain":
+                print(player.name_id ,"Jugador CAPTAAAAAAAAAAAAAAAAAAAAAAAAAAIN LOOOOOOOOOOOOOOOOOOOOK")
                 if card == player.cards[0]:
                     player.show[0] = player.cards[0]  #Reemplazo el show
                     player.cards[0] = " "
                     
                     if player.coins == 1:
                         player.coins -= 1
-                        player.coins += 1
+                        player_list[playern].coins += 1
                     else:
                         player.coins -= 2
-                        player.coins += 2
+                        player_list[playern].coins += 2
                 #Lose card 1
                 elif card == player.cards[1]: 
                     player.show[1] = player.cards[1] #Reemplazo el show
                     player.cards[1] = " "
                     if player.coins == 1:
                         player.coins -= 1
-                        player.coins += 1
+                        player_list[playern].coins += 1
                     else:
                         player.coins -= 2
-                        player.coins += 2
+                        player_list[playern].coins += 2
             screen_update.info()
         
         cardloose_label = tki.Label(cardloose_select, text=("Select card to lose " + str(player_list[playern].name_id)), font="times 15")
         cardloose_label.pack()
         cardname1 = str(player_list[playern].cards[0])
-        card1 = tki.Button(cardloose_select, text=(cardname1), command = lambda: cardloose(cardname1), font="consolas 12")
+        card1 = tki.Button(cardloose_select, text=(cardname1), command = lambda: cardloose(cardname1, player, player_list, name), font="consolas 12")
         card1.pack()
         cardname2 = str(player_list[playern].cards[1])
-        card2 = tki.Button(cardloose_select, text=(cardname2), command = lambda: cardloose(cardname2), font="consolas 12")
+        card2 = tki.Button(cardloose_select, text=(cardname2), command = lambda: cardloose(cardname1, player, player_list, name), font="consolas 12")
         card2.pack()
         cardloose_select.mainloop()
         
         player.cartas_reveladas.append(player.cards[carta_a_revelar])
         
-    def desafiar(self,player, player_list, playern, name):
+    def desafiar(self,player, player_list, playern, name): #player ES EL QUE DESAFIO AL player_list[playern]
             print(playern, "PLAYERN EN DESAFIAR---------------------")
             #print("hola soy al que desafiaron",player.name_id,player.cards[0], player.cards[1])
             #print("chao soy el que desafio al men",player2.name_id, player2.cards[0], player2.cards[1])
@@ -85,6 +90,16 @@ class Actions:
                 if action == "desafiar_none":
                     if name == "duke":
                         player_list[playern].coins +=3
+                    elif name == "captain":
+                        if player.coins == 1:
+                            player.coins -= 1
+                            player_list[playern].coins += 1
+                        else:
+                            player.coins -= 2
+                            player_list[playern].coins += 2
+                    elif name == "assassin":
+                        player.coins -= 3 
+                        self.quitar_carta(player, player_list, playern, name)
                     print("PASS desafiar")
                     return 0
                 elif action == "desafiar_p1":
@@ -222,31 +237,35 @@ class Actions:
         
         
     def steal(self,player, player_list, playern, name):
+        print(playern, "PLAYERN EN STEAL --------------------------------------------------")
+        
         screen_update = Screen_update(player_list)
         steal_panel = tki.Tk()
         steal_panel.title("STEAL")
         steal_panel.geometry("320x300")
+        
+        
         def action(action, target, playern, player_list, name):
             if playern == target:
                 print("AUTO STEAL BLOCKED")
             else:
                 steal_panel.destroy()
                 if action == "steal_p1":
-                    print("Player 1 DO STEAL TO ", playern + 1)
+                    print("Player ", playern + 1, " STEAL TO Player 1", )
                     #preguntar otra vez quien quiere dudarle la carta al que
                     #esta contraatacando, cambialo a botones
                     
                     self.desafiar(player_list[0], player_list, playern, name)
                 elif action == "steal_p2":
-                    print("Player 2 DO STEAL TO ", playern + 1)
+                    print("Player ", playern + 1, " STEAL TO Player 2", )
                     
                     self.desafiar(player_list[1], player_list, playern, name)
                 elif action == "steal_p3":
-                    print("Player 3 DO STEAL TO ", playern + 1)
+                    print("Player ", playern + 1, " STEAL TO Player 3", )
 
                     self.desafiar(player_list[2], player_list, playern, name)
                 elif action == "steal_p4":
-                    print("Player 4 DO STEAL TO ", playern + 1)
+                    print("Player ", playern + 1, " STEAL TO Player 4", )
 
                     self.desafiar(player_list[3], player_list, playern, name)
     
@@ -274,51 +293,72 @@ class Actions:
 
         return True
         
-        
-        
-        
-        
-        
-        
-        
-        """
-        
-        player_steal=int(input(F"{player_list[playern-1].name_id} type de number of the player you want to steal his coins"))
-        if player_list[playern - 1].name_id == player_list[player_steal].name_id:
-            while player_list[playern - 1].name_id == player_list[player_steal].name_id:
-                player_steal = int(
-                    input(F"{player_list[playern-1].name_id} type de number of the player you want to steal his coins"))
 
-        dudar = int(input("ingrese el numero del jugador que quiere dudarle, si no quiere nadie dudar"
-                          "entonces ingrese '5'\n"))
-        if player_list[playern - 1].name_id == player_list[dudar].name_id:
-            print("estas dentro?")
-            while player_list[playern - 1].name_id == player_list[dudar].name_id:
-                dudar = int(input("ingrese el numero del jugador que quiere dudarle, si nadie quiere dudar"
-                                  "entonces ingrese '5'\n"))
-        if dudar == 5:
-            print("nadie dudo prosigan jugando.")
-            contraatacar()
-            dudar()
-            if player_list[player_steal].coins >= 2:
-                player_list[playern-1].coins += 2
+    def assassinate(self,player, player_list, playern, name):
+        print(playern, "PLAYERN EN ASSASIN --------------------------------------------------")
+        
+        screen_update = Screen_update(player_list)
+        kill_panel = tki.Tk()
+        kill_panel.title("kill")
+        kill_panel.geometry("320x300")
+        
+        
+        def action(action, target, playern, player_list, name):
+            if playern == target:
+                print("AUTO kill BLOCKED")
             else:
-                player_list[playern - 1].coins += 1
-        else:
-            veredicto = self.desafiar(player_list[playern - 1], player_list[dudar], "captain")
-            if veredicto == True:
-                if player_list[player_steal].coins >= 2:
-                    player_list[playern - 1].coins += 2
-                else:
-                    player_list[playern - 1].coins += 1
-            else:
-                print("la jugada era falsa, la jugada no se seguira.")
-            """
+                kill_panel.destroy()
+                if action == "kill_p1":
+                    print("Player ", playern + 1, " kill TO Player 1", )
+                    #preguntar otra vez quien quiere dudarle la carta al que
+                    #esta contraatacando, cambialo a botones
+                    
+                    player.coins -= 3 
+                    self.quitar_carta(player_list[0], player_list, playern, name)
+            
+                elif action == "kill_p2":
+                    print("Player ", playern + 1, " kill TO Player 2", )
+                    
+                    player.coins -= 3 
+                    self.quitar_carta(player_list[1], player_list, playern, name)
+                    
+                elif action == "kill_p3":
+                    print("Player ", playern + 1, " kill TO Player 3", )
+                    
+                    player.coins -= 3 
+                    self.quitar_carta(player_list[2], player_list, playern, name)
+                    
+                elif action == "kill_p4":
+                    print("Player ", playern + 1, " kill TO Player 4", )
+                    
+                    player.coins -= 3 
+                    self.quitar_carta(player_list[3], player_list, playern, name)
+                    
+    
+                    
+        #PANEL DE COUNTER ACTION
+
+        kill_label = tki.Label(kill_panel, text="WHO are going to assassinate?", font="Consolas 15")
+        kill_label.pack()
+
+        #PLAYER WHO
+
+        kill_1 = tki.Button(kill_panel, text="Player 1", font="consolas 20", command = lambda: action("kill_p1", 0, playern, player_list, name))
+        kill_1.pack()
+        kill_2 = tki.Button(kill_panel, text="Player 2", font="consolas 20", command = lambda: action("kill_p2", 1, playern, player_list, name))
+        kill_2.pack()
+        kill_3 = tki.Button(kill_panel, text="Player 3", font="consolas 20", command = lambda: action("kill_p3", 2, playern, player_list, name))
+        kill_3.pack()
+
+        if len(player_list) == 4:
+            kill_4 = tki.Button(kill_panel, text="Player 4", font="consolas 20", command = lambda: action("kill_p4", 3, playern,player_list, name))
+            kill_4.pack()
 
 
+        kill_panel.mainloop()
 
-    def assassinte(self,player_list):
-        pass
+        return True
+
     def exchange(self,player_list):
         pass
 
